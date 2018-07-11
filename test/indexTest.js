fe6b024dc425bd5cb4938d3a2e6a6811082a3d3d
veronicacheung1105 <veronica.cheung1105@gmail.com>
2018-07-11 18:44:50 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,7 @@
 const expect = chai.expect;
 
 describe('index.js', function () {
-  describe('companyName', function () {
+  describe('Scuber', function () {
     it('is set as Scuber', function () {
       expect(companyName).to.equal('Scuber');
     });
